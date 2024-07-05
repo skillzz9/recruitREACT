d87aafd6735036f7e6f81022d0febdef3338f6ee
skillzz9 <hpos7501@uni.sydney.edu.au>
2024-07-05 15:57:24 +1000

    routes added and homepage complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import HomeCards from './components/Homecards';
-import JobListings from './components/JobListings';
-import ViewAllJobs from './components/ViewAllJobs';
 
+import HomePage from './pages/HomePage';
+import JobsPage from './pages/JobsPage';
+
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+
+const router = createBrowserRouter(
+
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+    <Route index element={<HomePage />} />
+    <Route index path='/jobs' element={<JobsPage />} />
+  </Route>
+));
 const App = () => {
-  return (
-  <>
-  
-  <Navbar></Navbar>
-  <Hero title="Become a React Dev" subtitle="Find the React job that fits your skills and needs"></Hero>
-  <HomeCards> </HomeCards>
-  <JobListings></JobListings>
-  <ViewAllJobs></ViewAllJobs>
-  
-  </>);
+  return ( <RouterProvider router={router} />);
   
 };
 
