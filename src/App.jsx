a459187f70233123817994a09d244e5cab2fb806
skillzz9 <hpos7501@uni.sydney.edu.au>
2024-07-07 11:34:24 +1000

    ability to add jobs and is integrated into the backend

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,37 @@ import JobsPage from './pages/JobsPage';
 import MainLayout from './layouts/MainLayout';
 import NotFoundPage from './pages/NotFoundPage';
 import SingleJobPage, { jobLoader } from './pages/SingleJobPage';
+import AddJobsPage from './pages/AddJobsPage';
 
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
 
-const router = createBrowserRouter(
-
-  createRoutesFromElements(
-    <Route path='/' element={<MainLayout />}>
-    <Route index element={<HomePage />} />
-    <Route path='/jobs' element={<JobsPage />} />
-    <Route path='/jobs/:id' element={<SingleJobPage/>} loader={jobLoader}/>
-    <Route path='*' element={<NotFoundPage />} />
-  
-
-  </Route>
-));
 const App = () => {
+  const addJob = async (newJob) =>{
+    const res = await fetch('/api/jobs',{
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newJob),
+    }
+    );
+    return;
+
+  }
+  
+  const router = createBrowserRouter(
+  
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path='/jobs/:id' element={<SingleJobPage/>} loader={jobLoader}/>
+      <Route path='/add-job' element={<AddJobsPage addJobSubmit={addJob}/>}/>
+      <Route path='*' element={<NotFoundPage />} />
+    
+  
+    </Route>
+  ));
   return ( <RouterProvider router={router} />);
   
 };
--- src/pages/SingleJobPage.jsx
+++ src/pages/SingleJobPage.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 // import { useState, useEffect } from 'react';
 import { useParams, useLoaderData } from 'react-router-dom';
 import Spinner from '../components/Spinner';
-import { FaArrowLeft } from 'react-icons/fa';
+import { FaArrowLeft, FaMapMarker } from 'react-icons/fa';
 
 const SingleJobPage = () => {
   const { id } = useParams();
@@ -50,9 +50,7 @@ const SingleJobPage = () => {
               <div
                 className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
               >
-                <i
-                  className="fa-solid fa-location-dot text-lg text-orange-700 mr-2"
-                ></i>
+                <FaMapMarker className='text-orange-700 mr-1' />
                 <p className="text-orange-700">{job.location}</p>
               </div>
             </div>
@@ -97,10 +95,10 @@ const SingleJobPage = () => {
 
             <div className="bg-white p-6 rounded-lg shadow-md mt-6">
               <h3 className="text-xl font-bold mb-6">Manage Job</h3>
-              <a
-                href="/add-job.html"
+              <Link
+                to={`/jobs/edit/${job.id}`}
                 className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-                >Edit Job</a
+                >Edit Job</Link
               >
               <button
                 className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
